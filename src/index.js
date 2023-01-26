e5d5048214da729dafab35ff8e8c0ccaca0f5bd1
mmeitneriumm <mmeitneriumm@gmail.com>
2023-01-26 16:10:51 +0300

    json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Post from './Post'
 import './styles/styles.css'
+import json from './assets/test.json'
 
 const post = new Post('title')
 
+console.log('json: ', json)
 console.log(post.toString())
