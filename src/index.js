fc546c18c3fa5a69de4a8952b1e49c2a16e735d1
mmeitneriumm <mmeitneriumm@gmail.com>
2023-01-30 14:56:15 +0300

    csv

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import Post from './Post'
 import './styles/styles.css'
 import json from './assets/test.json'
 import uplabLogo from './assets/uplab-logo.jpg'
+import xml from './assets/plant_catalog.xml'
+import csv from './assets/example.csv'
 
 const post = new Post('title', uplabLogo)
 
 console.log('json: ', json)
+console.log('XML: ', xml)
+console.log('CSV: ', csv)
 console.log(post.toString())
